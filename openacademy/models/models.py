6db6f31a857d4edee57a53750dd6a3024f435fc0
Gilberto Peraza <gperaza@pyansa.com.mx>
2020-09-08 14:36:10 -0600

    [REF] computed field and default

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import time 
+def get_uid(self, *a):
+    return self.env.uid
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -8,16 +11,22 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    # responsible_id = fields.Many2one('res.users',
+        # ondelete='set null', string="Responsible", index=True)
     responsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible", index=True)
-    session_ids = fields.One2many('openacademy.session','course_id')
+        ondelete='set null',
+        # default=lambda self, *a: self.env.uid)    
+        default=get_uid)
+    session_ids = fields.One2many('openacademy.session','course_id')    
 
 class Session(models.Model):
     _name = 'openacademy.session'
     _description="Clase Session para Curso"
     
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
+    # datetime_test= fields.Datetime(default=lambda *a: time.strftime('%Y-%m-%d %H:%M:%S'))
+    datetime_test= fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     
@@ -27,20 +36,17 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course',
          ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")  
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active = fields.Boolean(default=True)
 
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        # for record  in self.filtered(lambda r: r.seats):
+            # record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
 
-
-
-
-
-
-
-
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+        # import pdb; pdb.set_trace()
+        for record in self:
+            if not record.seats:
+                record.taken_seats = 0.0
+            else:
+                record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
